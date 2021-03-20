62a6ac58ba2b29b2cf876e1f3af0b95b5c4e571d
Aspyro95 <damien@ghysweb.be>
2021-03-20 17:23:23 +0100

    Fin de journée 3 : - Création citoyen fonctionnelle - Package documenté

---
+++ src/main/java/dev/aspyro/worldsgenerator/comportement/generateurs/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Le package contenant les les générateurs d'informations
+ *
+ * @author Aspyro
+ * @version %I%, %G%
+ * @see dev.aspyro.worldsgenerator.comportement.generateurs.GenerateurPrenom;
+ * @see dev.aspyro.worldsgenerator.comportement.generateurs.GenerateurNom;
+ */
+
+package dev.aspyro.worldsgenerator.comportement.generateurs;
